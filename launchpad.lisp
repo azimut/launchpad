69aaff8acce3751e7b12341677ac601ccacb4bb7
azimut <azimut.github@protonmail.com>
2020-05-22 16:38:18 -0300

    minor

--- launchpad.lisp
+++ launchpad.lisp
@@ -2,11 +2,11 @@
 
 (in-package #:launchpad)
 
-(defun random-between (from to)
-  (let ((n (- to from)))
-    (assert (< from to) (from to))
-    (a:random-elt
-     (a:iota (+ 1 n) :start from))))
+(defun list-to-mat (list)
+  (->> list
+       (substitute 0 '_)
+       (substitute 1 'X)
+       (apply #'mat)))
 
 (defun command (raw-midi)
   (cl-rtmidi:with-midi-oss-out (cl-rtmidi:*default-midi-out-stream* "/dev/midi1")
@@ -17,7 +17,7 @@
 (progn
   (command '(176 0 0))
   (command (list 144 (+ (random 8) (* (random 8) 16)) #b0110010))
-  (command (list 176 (random-between 104 (+ 104 8)) #b0110010)))
+  (command (list 176 (+ (random 8) 104)               #b0110010)))
 
 (defparameter *five*
   '(_ _ _ _ X _ _ _
@@ -28,22 +28,6 @@
     _ _ _ _ X _ _ _
     _ _ _ _ X _ _ _
     _ _ _ X X X _ _))
-
-(defparameter *five*
-  '(_ _ _ _ _ _ X _
-    _ _ _ _ _ _ X X
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ _ _ _ _ _ _ _
-    _ X X _ _ _ _ _
-    _ X _ _ _ _ _ _))
-
-(defun list-to-mat (list)
-  (->> list
-       (substitute 0 '_)
-       (substitute 1 'X)
-       (apply #'mat)))
 
 (defun test-draw ()
   "OUT send a blink color for 2A pattern"
@@ -59,7 +43,6 @@
       (dotimes (col 8)
         (when (= 1 (mcref m col row))
           (command (list 144 (+ row (* col 16)) #b0110010)))))))
-
 
 (defun test-input ()
   "IN debug print what is pressed"
